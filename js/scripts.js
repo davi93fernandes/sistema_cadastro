d3d120a49000e28f424cdd15ca93b070452f649f
davi93fernandes <davi.a.fernandes@hotmail.com>
2023-10-14 02:33:54 -0300

    Ajusta html e css, inicia script

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", () => {
+    const elementoBotao = document.getElementById("btn-register")
+    elementoBotao.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    console.log(elementoBotao)
+})
+})
+
+let form = document.getElementById("register-form")
+
+
